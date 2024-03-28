5c696dbf5248fd616477f47484a6772a7484237e
Ekaterina Erokhina <katerina.shimshek@gmail.com>
2024-03-28 17:26:01 +0300

    усечение строки

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,12 @@
+// function truncate(str, maxlength) {
+//   if (str.length > maxlength) {
+//     return str.slice(0, maxlength - 1) + "…"
+//   } else {
+//     str
+//   }
+// }; почему этот код не работает? это же то же самое, что и через тернарный оператор, или нет?
+
 function truncate(str, maxlength) {
-  // ваш код...
-}
+  return (str.length > maxlength) ?
+    str.slice(0, maxlength - 1) + '…' : str;
+};
