3d4a22451ccf9ca3da6cb57455b27fd907770b4c
Ekaterina Erokhina <katerina.shimshek@gmail.com>
2024-03-31 19:47:33 +0300

    задачи 1-3 модуля 2

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,18 @@
-function sumSalary(salaries) {
-  // ваш код...
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
 }
+
+function sumSalary(salaries) {
+  let sum = 0;
+
+  for (key in salaries) {
+    if (typeof (salaries[key]) === 'number' && !isNaN(salaries[key]) && isFinite(salaries[key]))
+      sum += salaries[key];
+  }
+  return sum;
+};
