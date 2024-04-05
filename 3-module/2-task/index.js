56cf021756e026f434885713c846f9c08622f1a9
Ekaterina Erokhina <katerina.shimshek@gmail.com>
2024-04-05 18:29:04 +0300

    задачи 1-5 модуля

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,8 @@
+let arr = [5, 3, 8, 1];
+
 function filterRange(arr, a, b) {
-  // ваш код...
+  let newArr = arr.filter(item => item >= a && item <= b);
+  return newArr;
 }
+
+let filtered = filterRange(arr, 1, 4);
--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,11 @@
 function getMinMax(str) {
-  // ваш код...
+  const numbers = str
+    .split(' ')
+    .filter(item => !isNaN(parseFloat(item)))
+    .map(item => parseFloat(item));
+
+  const min = Math.min(...numbers);
+  const max = Math.max(...numbers);
+
+  return { min, max };
 }
